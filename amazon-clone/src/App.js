5c6b5170cacbd9358df82c808aafd253217dd627
Ht <htcvhs971@gmail.com>
2022-09-26 15:47:44 +0200

    create Header Component:first structure

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
 	return (
 		//BEM:app
 		<div className="app">
 			<h1>Test App</h1>
+			<Header />
+			{/* Home */}
 		</div>
 	);
 }
