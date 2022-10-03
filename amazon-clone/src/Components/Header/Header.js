93a20868676d5ae86d990544b8e9882cea0622f8
Ht <htcvhs971@gmail.com>
2022-10-03 09:45:35 +0200

    Header Localisation

--- amazon-clone/src/Components/Header/Header.js
+++ amazon-clone/src/Components/Header/Header.js
@@ -9,6 +9,16 @@ function Header() {
 				{/* TODO:Make country dynamic */}
 				<p className="header__country">.fr</p>
 			</div>
+
+			{/* TODO:Make localisation dynamic */}
+			<div className="header__localisation">
+				<div className="header__locator"></div>
+				{/* Or make the header__locator like the header__logo made w/ a img */}
+				<div className="header__optionsLocator">
+					<span className="header__optionLineOne">Bonjour</span>
+					<span className="header__optionLineTwo">Entrez votre adresse</span>
+				</div>
+			</div>
 		</div>
 	);
 }
