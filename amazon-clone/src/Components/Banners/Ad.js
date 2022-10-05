23949c619deaa86a34b401aec61509660e88a462
Ht <htcvhs971@gmail.com>
2022-10-05 13:30:53 +0200

    Bug: img to Ad__banner

--- amazon-clone/src/Components/Banners/Ad.js
+++ amazon-clone/src/Components/Banners/Ad.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './Ad.css';
+import '../Banners/Ad.css';
 
 function Ad() {
 	return (
-		<div className="home__banner">
-			<img src="https://m.media-amazon.com/images/I/61oQwPh+arL._SX3000_.jpg" alt="" className="home__image" />
-			<img src="" alt="" className="home__image" />
-			<img src="" alt="" className="home__image" />
+		<div className="ad__banner">
+			<img src="https://m.media-amazon.com/images/I/717wlrF129L._SX3000_.jpg" alt="ad" className="ad__image" />
 		</div>
 	);
 }
--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -6,27 +6,29 @@ function Home() {
 	return (
 		<div>
 			<div className="home">
-				<Ad />
-				<div className="home__main">
-					<div className="home__row">
-						{/* Product */}
-						{/* Product */}
-						{/* Product */}
-						{/* SignInCard */}
-					</div>
-					<div className="home__row">
-						{/* Product */}
-						{/* MultiProducts>Product */}
-						{/* Product */}
-						{/* MultiProducts>Product */}
-					</div>
-					<div className="home__row">{/* BannerProduct.TopSellers_Sport>Product */}</div>
-					<div className="home__row">{/* BannerProduct.repurchased>Product */}</div>
-					<div className="home__row">
-						{/* MultiProducts>Product */}
-						{/* Product */}
-						{/* MultiProducts>Product */}
-						{/* MultiProducts>Product */}
+				<div className="home__container">
+					<Ad />
+					<div className="home__main">
+						<div className="home__row">
+							{/* Product */}
+							{/* Product */}
+							{/* Product */}
+							{/* SignInCard */}
+						</div>
+						<div className="home__row">
+							{/* Product */}
+							{/* MultiProducts>Product */}
+							{/* Product */}
+							{/* MultiProducts>Product */}
+						</div>
+						<div className="home__row">{/* BannerProduct.TopSellers_Sport>Product */}</div>
+						<div className="home__row">{/* BannerProduct.repurchased>Product */}</div>
+						<div className="home__row">
+							{/* MultiProducts>Product */}
+							{/* Product */}
+							{/* MultiProducts>Product */}
+							{/* MultiProducts>Product */}
+						</div>
 					</div>
 				</div>
 			</div>
