1df1823b56f8e4dcf684eff07bb9f8e326bf36a9
Ht <htcvhs971@gmail.com>
2022-10-05 11:42:36 +0200

    Deleted home__container

--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -5,35 +5,33 @@ function Home() {
 	return (
 		<div>
 			<div className="home">
-				<div className="home__container">
-					<div className="home__head">
-						<div className="home__banner">
-							<img src="" alt="" className="home__image" />
-							<img src="" alt="" className="home__image" />
-							<img src="" alt="" className="home__image" />
-						</div>
+				<div className="home__head">
+					<div className="home__banner">
+						<img src="" alt="" className="home__image" />
+						<img src="" alt="" className="home__image" />
+						<img src="" alt="" className="home__image" />
 					</div>
-					<div className="home__main">
-						<div className="home__row">
-							{/* Product */}
-							{/* Product */}
-							{/* Product */}
-							{/* SignInCard */}
-						</div>
-						<div className="home__row">
-							{/* Product */}
-							{/* MultiProducts>Product */}
-							{/* Product */}
-							{/* MultiProducts>Product */}
-						</div>
-						<div className="home__row">{/* BannerProduct.TopSellers_Sport>Product */}</div>
-						<div className="home__row">{/* BannerProduct.repurchased>Product */}</div>
-						<div className="home__row">
-							{/* MultiProducts>Product */}
-							{/* Product */}
-							{/* MultiProducts>Product */}
-							{/* MultiProducts>Product */}
-						</div>
+				</div>
+				<div className="home__main">
+					<div className="home__row">
+						{/* Product */}
+						{/* Product */}
+						{/* Product */}
+						{/* SignInCard */}
+					</div>
+					<div className="home__row">
+						{/* Product */}
+						{/* MultiProducts>Product */}
+						{/* Product */}
+						{/* MultiProducts>Product */}
+					</div>
+					<div className="home__row">{/* BannerProduct.TopSellers_Sport>Product */}</div>
+					<div className="home__row">{/* BannerProduct.repurchased>Product */}</div>
+					<div className="home__row">
+						{/* MultiProducts>Product */}
+						{/* Product */}
+						{/* MultiProducts>Product */}
+						{/* MultiProducts>Product */}
 					</div>
 				</div>
 			</div>
