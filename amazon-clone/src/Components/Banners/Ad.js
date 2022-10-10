903e628b68e4273f3c0676eb5a6b44759b517939
Ht <htcvhs971@gmail.com>
2022-10-10 10:55:55 +0200

    Slider

--- amazon-clone/src/Components/Banners/Ad.js
+++ amazon-clone/src/Components/Banners/Ad.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import './Ad.css';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
 function Ad() {
+	const baseUrl = 'https://m.media-amazon.com/images/I/';
+	const sliderDatas = [
+		{
+			id: 'slider__1',
+			image: `${baseUrl}71TSjqNBwFL._SX3000_.jpg`,
+			alt: 'flash',
+		},
+		{
+			id: 'slider__2',
+			image: `${baseUrl}717wlrF129L._SX3000_.jpg`,
+			alt: 'the lord of the ring',
+		},
+		{
+			id: 'slider__3',
+			image: `${baseUrl}71hE6KmyCIL._SX3000_.jpg`,
+			alt: 'amazon pack',
+		},
+		{
+			id: 'slider__4',
+			image: `${baseUrl}61oQwPh+arL._SX3000_.jpg`,
+			alt: 'exclusiv offer',
+		},
+	];
+
 	return (
 		<Carousel>
-			<div className="home__banner">
-				<img src="https://m.media-amazon.com/images/I/61oQwPh+arL._SX3000_.jpg" alt="exclusiv offer" className="home__image" />
-				{/* TODO: Make the img dynamic */}
-				<img src="https://m.media-amazon.com/images/I/717wlrF129L._SX3000_.jpg" alt="the lord of the ring" className="home__image" />
-				<img src="https://m.media-amazon.com/images/I/71hE6KmyCIL._SX3000_.jpg" alt="amazon pack" className="home__image" />
-				<img src="https://m.media-amazon.com/images/I/71TSjqNBwFL._SX3000_.jpg" alt="flash" className="home__image" />
-			</div>
+			{sliderDatas.map((slide) => (
+				// Key car c'est une méthode map
+				<div key={slide.id} className="home__image">
+					<img src={slide.image} alt={slide.alt} />
+				</div>
+			))}
 		</Carousel>
 	);
 }
